a94e8e90005b6b21580ea426de4e546a9ad8b3bf
themildaisalie <144637581+themildaisalie@users.noreply.github.com>
2023-09-10 20:26:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const params = new URLSearchParams(document.location.search);
 const s = params.get("place");
 const p = params.get("serverid")
-const link = "roblox://experiences/start?placeId="+s+"&serverId="+p
+const link = "roblox://experiences/start?placeId="+s+"&gameInstanceId="+p
 console.log(link)
 window.location.href = link;
